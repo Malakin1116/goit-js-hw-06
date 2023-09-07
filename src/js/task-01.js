aa22125aa80420012e1324a7a39199220282d9bb
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-07 14:57:06 +0300

    Update task-01.js

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -3,6 +3,13 @@
 // 1)
 const ulElement = document.querySelector('ul#categories');
 const categoryItems = ulElement.querySelectorAll('li.item');
-console.log('Кількість категорій:', categoryItems.length);
+console.log('Number of categories:', categoryItems.length);
 
 // 2)
+categoryItems.forEach((item)=>{
+    const categoryTitle = item.querySelector("h2");
+    const categoryListItems = item.querySelectorAll("ul > li");
+
+    console.log("Category:", categoryTitle.textContent);
+    console.log("Elements:", categoryListItems.length);
+});
