d004196b8add2429323a5a9629d5a2136b68f7b8
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-07 14:18:20 +0300

    Update task-01.js

---
+++ src/js/task-01.js
@@ -0,0 +1,8 @@
+
+
+// 1)
+const ulElement = document.querySelector('ul#categories');
+const categoryItems = ulElement.querySelectorAll('li.item');
+console.log('Кількість категорій:', categoryItems.length);
+
+// 2)
