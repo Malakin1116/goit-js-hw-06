08e2c8cc0029380e08d2cc949cc5409266b34bfe
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-08 14:29:56 +0300

    55

--- js/task-05.js
+++ js/task-05.js
@@ -2,7 +2,7 @@ const nameInput = document.querySelector("#name-input");
 const nameOutput = document.querySelector("#name-output");
 
 nameInput.addEventListener("input" , () => {
-    const inputValue = nameInput.value();
+    const inputValue = nameInput.value;
 
     nameOutput.textContent = inputValue ? inputValue : 'Anonymous';
 });
