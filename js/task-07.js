b759a497e6db6f92fd628924bb71d2472b6d209e
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-08 18:08:33 +0300

    999

---
+++ js/task-07.js
@@ -0,0 +1,10 @@
+const fontSizeControl = document.getElementById("font-size-control");
+const textElement = document.getElementById("text");
+
+textElement.style.fontSize = fontSizeControl.value + "px";
+
+fontSizeControl.addEventListener("input", () => {
+  const fontSize = fontSizeControl.value + "px";
+  textElement.style.fontSize = fontSize;
+  
+});
--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,23 @@
 
+const loginForm = document.querySelector('.login-form');
+
+loginForm.addEventListener('submit', function (event) {
+  event.preventDefault(); 
+  
+  const emailInput = this.elements.email;
+  const passwordInput = this.elements.password;
+
+  if (emailInput.value.trim() === '' || passwordInput.value.trim() === '') {
+    alert('Усі поля повинні бути заповнені');
+    return;
+  }
+
+  const formData = {
+    email: emailInput.value,
+    password: passwordInput.value,
+  };
+
+  console.log(formData);
+
+  this.reset(); 
+});
