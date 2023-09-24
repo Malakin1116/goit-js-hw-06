948bb6d2ff5dd8555432503b9a098c8cc0dd29e9
Malakin1116 <130695935+Malakin1116@users.noreply.github.com>
2023-09-24 18:10:50 +0300

    ++

--- js/task-01.js
+++ js/task-01.js
@@ -1,19 +1,20 @@
-
-
-const categoriesUll = document.querySelector('#categories');
+const categoriesUll = document.querySelector("#categories");
 const numOfCategories = categoriesUll.children.length;
-const categoryItems = categoriesUll.querySelectorAll('.item');
+
+console.log(`Number of categories: ${numOfCategories}`);
+
+const categoryItems = categoriesUll.querySelectorAll(".item");
 const categoryInfo = [];
 
 categoryItems.forEach(item => {
-    const categoryName = item.querySelector('h2').textContent;
-    const numElements = item.querySelectorAll('ul li').length;
+    const categoryName = item.querySelector("h2").textContent;
+    const numElements = item.querySelectorAll("ul>li").length;
+
+    console.log(`Category: ${categoryName}`);
+    console.log(`Elements: ${numElements}`);
     
     categoryInfo.push({
         category: categoryName,
         elements: numElements,
     });
 });
-
-console.log(`Number of categories: ${numOfCategories}`);
-console.log(categoryInfo);
